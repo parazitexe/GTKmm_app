dae37a1c5b7a26e539d81714b771fabdec43be82
Parazitexe <parazitexe@gmail.com>
2018-09-06 15:12:38 +0300

    another RTSP server
    add another solution for RTSP server

---
+++ node-rtsp-server/app.js
@@ -0,0 +1,20 @@
+var rtsp = require('rtsp-server')
+ 
+var server = rtsp.createServer(function (req, res) {
+  console.log(req.method, req)
+ 
+//  switch (req.method) {
+//    case 'OPTIONS':
+//      res.setHeader('Public', 'OPTIONS');
+//      break
+//    default:
+//      res.statusCode = 501 // Not implemented
+//  }
+ 
+  res.end() // will echo the CSeq header used in the request
+})
+ 
+server.listen(5000, function () {
+  var port = server.address().port
+  console.log('RTSP server is running on port:', port)
+})
